bd1e8e1e2c7b2986c5dfca283af288d1eabd90a2
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-19 18:55:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I love you!".upcase()
+    return "I LOVE YOU TOO PUMPKIN!"  
   else
     return "NO, NOT SINCE 1938!"
   end
